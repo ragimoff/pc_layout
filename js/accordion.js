b731756b5e190256d266a7dcea6f709993b183eb
Ragim.Guseynov <41242742+ragimoff@users.noreply.github.com>
2022-04-24 19:13:45 +0400

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,18 @@
+const contents = document.querySelectorAll('.program-line__content');
+
+contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title');
+		
+    title.addEventListener('click', () => {
+        contents.forEach((elems) => {
+					const descr = elems.querySelector('.program-line__descr');
+					
+            if (elems == elem) {
+                descr.classList.toggle('active');
+            }
+            else {
+                descr.classList.remove('active');
+            }
+        });
+    })
+})
